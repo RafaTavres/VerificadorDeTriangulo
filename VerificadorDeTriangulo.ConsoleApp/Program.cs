65c135c6bbac3fd19e82d6327d2218cfbbfc627b
Rafael S Tavares <ragbriros@gmail.com>
2023-04-05 18:43:09 -0300

    Add project files.

---
+++ VerificadorDeTriangulo.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace VerificadorDeTriangulo.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----Classificador de Triangulos----");
+            string resposta = "";
+            while (resposta.ToUpper() != "N")
+            {
+                Triangulo triangulo = new Triangulo();
+                PegaDadosDoTriangulo(triangulo);
+                Console.WriteLine("O Triangulo é " + triangulo.VerificaTipoDeTriangulo());
+
+                Console.WriteLine("Continuar S/N");
+                resposta = Console.ReadLine();
+            }
+        }
+
+        private static void PegaDadosDoTriangulo(Triangulo triangulo)
+        {
+            Console.WriteLine("- Qual o Tamanho do Lado 1?");
+            triangulo.lado_1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("- Qual o Tamanho do Lado 2?");
+            triangulo.lado_2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("- Qual o Tamanho do Lado 3?");
+            triangulo.lado_3 = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ VerificadorDeTriangulo.ConsoleApp/Triangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificadorDeTriangulo.ConsoleApp
+{
+    internal class Triangulo
+    {
+        public double lado_1;
+        public double lado_2;
+        public double lado_3;
+       
+        public string VerificaTipoDeTriangulo()
+        {
+            if (VerificaLadoValido(lado_1, lado_2, lado_3) is false)
+            {
+                return "Triangulo Inválido";
+            }
+
+            else if (lado_1 == lado_2 & lado_1 == lado_3 & lado_2 == lado_3)
+            {
+                return "Triangulo Equilátero";
+            }
+
+            else if (lado_1 == lado_2 | lado_1 == lado_3 | lado_2 == lado_3)
+            {
+                return "Isóceles";
+
+            }
+
+            else if (lado_1 != lado_2 & lado_1 != lado_3 & lado_2 != lado_3)
+            {
+                return "Escaleno";
+
+            }else
+                return "Triangulo Inválido";
+        }
+        public static bool VerificaLadoValido(double Lado_1, double Lado_2, double Lado_3)
+        {
+            double SomaLados1e2 = Lado_1 + Lado_2;
+            double SomaLados1e3 = Lado_1 + Lado_3;
+            double SomaLados2e3 = Lado_2 + Lado_3;
+
+            if (SomaLados1e2 < Lado_3 | SomaLados1e3 < Lado_2 | SomaLados2e3 < Lado_1)
+            {
+                return false;
+            }
+            if (Lado_1 == 0 | Lado_2 == 0 | Lado_3 == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
